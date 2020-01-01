cd994e9a0d58050785fee8518d0e13770d6e48dd
bthakr1 <44014462+bthakr1@users.noreply.github.com>
2020-01-01 01:14:55 -0600

    Initial Commit

---
+++ jsonRead/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  jsonRead
+//
+//  Created by Bhupendrasinh Thakre on 1/1/20.
+//  Copyright © 2020 Bhupendrasinh Thakre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
